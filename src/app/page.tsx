4d13ee935025b2ca0b764aa15596219e7193cafb
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-01-08 17:27:01 -0300

    selectors and states in Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen flex justify-center items-center flex-col gap-4">
-      <h1 className="font-bold text-5xl">Título</h1>
-      <h2 className="text-4xl text-purple font-extralight">Subtítulo</h2>
-      <p className="text-2xl font-thin text-center">
+      <h1 
+        className="font-bold text-5xl flex items-center gap-3 before:h-full before:w-0.5 before:bg-sky-500 before:flex">
+        Título
+      </h1>
+      <h2 
+        className="text-4xl text-purple font-extralight">
+        Subtítulo
+      </h2>
+      <p 
+        className="text-2xl font-thin text-center"
+      >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum at perspiciatis voluptates? Consequatur neque, voluptas officiis inventore voluptates magnam porro!
       </p>
+
+      <button 
+        className="bg-sky-500 text-xl font-medium px-4 py-2 mt-6 rounded-md enabled:hover:bg-sky-600 transition disabled:opacity-80 disabled:cursor-not-allowed"
+      >
+        Sign In
+      </button>
     </div>
   )
 }
