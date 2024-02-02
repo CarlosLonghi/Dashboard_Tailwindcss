1af91e1c51fc122d53e4fdf4fb68c9bf20f41546
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-02-02 14:09:43 -0300

    fix: refactor component TabItem

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -5,10 +5,9 @@ import * as Tabs from '@radix-ui/react-tabs'
 export interface TabItemProps {
   value: string
   title: string
-  isSelected?: boolean
 }
 
-export function TabItem({ value, title, isSelected = false }: TabItemProps) {
+export function TabItem({ value, title }: TabItemProps) {
   return (
     <Tabs.Trigger
       className="group relative px-1 pb-2 text-sm font-medium text-zinc-500 hover:text-emerald-600 data-[state=active]:text-emerald-600"
